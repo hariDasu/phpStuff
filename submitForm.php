0f02aa2ab96ac98dd9346d7f2e7dffdf716a7bbc
Srihari Rao <harirao3@gmail.com>
2013-10-14 15:05:03 -0400

    added sessions not doing much with it.

---
+++ submitForm.php
@@ -0,0 +1,53 @@
+<?php 
+include 'index.php';
+
+
+$username=$_POST['username'];
+$password=$_POST['password'];
+$email=$_POST['email'];
+
+
+$_SESSION['userName'] = "$username";
+
+
+if ($username == 'cleverGuy' && $password == 'cleverPassword' && $email=='clever@email.com')
+{
+$goToActivity = new activity(activity);
+
+
+
+$accessActivity = $goToActivity->get();
+
+
+    printf ("<div class='text-center alert alert-success'>
+                Welcome to your account,<strong> ");
+    printf($_SESSION['userName']);
+
+    printf("</strong>! </div>");
+
+echo "$accessActivity";
+}else{
+
+
+	$html = file_get_contents('login.html');
+	echo "$html";
+    printf ("<div class='text-center alert alert-danger'>
+                <strong>Incorrect Login!</strong> Please try again!
+            </div>");
+		
+}
+		
+
+
+
+
+	
+
+
+
+
+
+
+
+
+?>
